358cc4e61f44cfa33928bacfb5ccbb8bac092937
jiyangzhao <zhaojiyang1996@gmail.com>
2021-03-03 22:42:47 -0500

    7_1&7_2

---
+++ Programming Language/Cpp/C++ Primer Plus - Solution/Chapter 7/Problem_7_2.cpp
@@ -0,0 +1,7 @@
+void igor();
+float tofu(int);
+double mpg(double, double);
+long summation(long*, int);
+double doctor(const string);
+void ofcourse(boss);
+string plot(map*);
---
+++ Programming Language/Cpp/C++ Primer Plus - Solution/Chapter 7/Problem_7_3.cpp
@@ -0,0 +1,5 @@
+void fullfill(int arr[], int len, int num) {
+    for (int i = 0; i < len; ++i) {
+        arr[i] = num;
+    }
+}
